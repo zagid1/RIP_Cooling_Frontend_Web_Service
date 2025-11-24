e1a607ce2d63a1acc4818a6b98c65e384b8abdfe
Zagid <osmanovzagid2005@gmail.com>
2025-11-24 04:56:38 +0300

    ..

--- coolSystem-Frontend/dev-dist/sw.js
+++ coolSystem-Frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5pna3iqu4mg"
+    "revision": "0.pofm500nkt"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
