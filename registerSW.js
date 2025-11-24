60ac4f72f1a40555b048ca87eb9309b9b695588e
Zagid <osmanovzagid2005@gmail.com>
2025-11-24 22:41:26 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/RIP_Frontend_Web_Service/sw.js', { scope: '/RIP_Frontend_Web_Service/' })})}
